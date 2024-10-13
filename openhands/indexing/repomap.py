9b271b4f21564ee2925b5884c3231acde5e912a4
Hoang Tran <descience.thh10@gmail.com>
2024-10-13 14:17:32 +0000

    implement repomap

--- openhands/indexing/repomap.py
+++ openhands/indexing/repomap.py
@@ -1,14 +1,33 @@
+import base64
 import os
 import re
+import warnings
+from collections import Counter, defaultdict, namedtuple
+from importlib import resources
 from pathlib import Path, PurePosixPath
+from typing import Any, Set
 
 import git
 import pathspec
+from diskcache import Cache
+from grep_ast import filename_to_lang
+from pygments.lexers import guess_lexer_for_filename
+from pygments.token import Token
+from pygments.util import ClassNotFound
+from tqdm import tqdm
+from tree_sitter_languages import get_language, get_parser
 
-from openhands.indexing.utils import get_token_count_from_text
+from openhands.indexing.utils import get_token_count_from_text, is_image_file
+
+warnings.simplefilter('ignore', category=FutureWarning)
+
+Tag = namedtuple('Tag', ('rel_fname', 'fname', 'line', 'name', 'kind'))
 
 
 class RepoMap:
+    CACHE_VERSION = 3
+    TAGS_CACHE_DIR = f'.aider.tags.cache.v{CACHE_VERSION}'
+
     def __init__(
         self,
         map_tokens=1024,
@@ -29,6 +48,10 @@ class RepoMap:
         self.aider_ignore_file = Path(aider_ignore_file)
         self.aider_ignore_ts = 0
 
+        self.load_tags_cache()
+        self.cache_missing = False
+        self.warned_files: Set = set()
+
     def get_history_aware_repo_map(self, messages: list) -> str:
         full_messages_text = self.get_full_messages_text(messages)
         mentioned_fnames = self.get_file_mentions(full_messages_text)
@@ -45,8 +68,352 @@ class RepoMap:
 
         return repo_map_content
 
-    def get_repo_map(self, other_files, mentioned_fnames=None, mentioned_idents=None):
-        raise NotImplementedError
+    def get_repo_map(
+        self, other_files, mentioned_fnames=None, mentioned_idents=None
+    ) -> str:
+        if self.max_map_tokens <= 0:
+            return ''
+        if not other_files:
+            return ''
+        if not mentioned_fnames:
+            mentioned_fnames = set()
+        if not mentioned_idents:
+            mentioned_idents = set()
+
+        max_map_tokens = self.max_map_tokens
+
+        try:
+            files_listing = self.get_ranked_tags_map(
+                other_files,
+                max_map_tokens,
+                mentioned_fnames,
+                mentioned_idents,
+            )
+            print(files_listing)  # FIXME: remove this line
+        except RecursionError:
+            self.log_error('Disabling repo map, git repo too large?')
+            self.max_map_tokens = 0
+            return ''
+
+        # TODO:
+        raise NotImplementedError('This part of the code is not implemented yet.')
+
+    def get_ranked_tags_map(
+        self,
+        other_fnames=None,
+        max_map_tokens=None,
+        mentioned_fnames=None,
+        mentioned_idents=None,
+    ):
+        # FIXME: implement cache if needed
+        result = self.get_ranked_tags_map_uncached(
+            other_fnames, max_map_tokens, mentioned_fnames, mentioned_idents
+        )
+        return result
+
+    def get_ranked_tags_map_uncached(
+        self,
+        other_fnames=None,
+        max_map_tokens=None,
+        mentioned_fnames=None,
+        mentioned_idents=None,
+    ):
+        if not other_fnames:
+            other_fnames = list()
+        if not max_map_tokens:
+            max_map_tokens = self.max_map_tokens
+        if not mentioned_fnames:
+            mentioned_fnames = set()
+        if not mentioned_idents:
+            mentioned_idents = set()
+
+        ranked_tags = self.get_ranked_tags(
+            other_fnames, mentioned_fnames, mentioned_idents
+        )
+        # TODO: implement the rest of the code
+        print(ranked_tags)  # FIXME: remove this line
+        raise NotImplementedError('This part of the code is not implemented yet.')
+
+    def get_ranked_tags(self, other_fnames, mentioned_fnames, mentioned_idents):
+        import networkx as nx
+
+        defines = defaultdict(set)
+        references = defaultdict(list)
+        definitions = defaultdict(set)
+
+        personalization = dict()
+
+        fnames_set = set(other_fnames)
+        fnames = sorted(fnames_set)
+
+        # Default personalization for unspecified files is 1/num_nodes
+        # https://networkx.org/documentation/stable/_modules/networkx/algorithms/link_analysis/pagerank_alg.html#pagerank
+        personalize = 10 / len(fnames)
+
+        if self.cache_missing:
+            fnames = tqdm(fnames)
+        self.cache_missing = False
+
+        for fname in fnames:
+            if not Path(fname).is_file():
+                if fname not in self.warned_files:
+                    if Path(fname).exists():
+                        self.log_error(
+                            f"Repo-map can't include {fname}, it is not a normal file"
+                        )
+                    else:
+                        self.log_error(
+                            f"Repo-map can't include {fname}, it no longer exists"
+                        )
+
+                self.warned_files.add(fname)
+                continue
+
+            # dump(fname)
+            rel_fname = self.get_relative_fname(fname)
+
+            if fname in mentioned_fnames:
+                personalization[rel_fname] = personalize
+
+            tags = list(self.get_tags(fname, rel_fname))
+
+            for tag in tags:
+                if tag.kind == 'def':
+                    defines[tag.name].add(rel_fname)
+                    key = (rel_fname, tag.name)
+                    definitions[key].add(tag)
+
+                if tag.kind == 'ref':
+                    references[tag.name].append(rel_fname)
+
+        ##
+        # dump(defines)
+        # dump(references)
+        # dump(personalization)
+
+        if not references:
+            references = defaultdict(list)
+            for k, v in defines.items():
+                references[k] = list(v)
+
+        idents = set(defines.keys()).intersection(set(references.keys()))
+
+        G = nx.MultiDiGraph()
+
+        for ident in idents:
+            definers = defines[ident]
+            if ident in mentioned_idents:
+                mul = 10
+            else:
+                mul = 1
+            for referencer, num_refs in Counter(references[ident]).items():
+                for definer in definers:
+                    # if referencer == definer:
+                    #    continue
+                    G.add_edge(referencer, definer, weight=mul * num_refs, ident=ident)
+
+        if not references:
+            pass
+
+        if personalization:
+            pers_args = dict(personalization=personalization, dangling=personalization)
+        else:
+            pers_args = dict()
+
+        try:
+            ranked = nx.pagerank(G, weight='weight', **pers_args)
+        except ZeroDivisionError:
+            return []
+
+        # distribute the rank from each source node, across all of its out edges
+        ranked_definitions: Any = defaultdict(float)
+        for src in G.nodes:
+            src_rank = ranked[src]
+            total_weight = sum(
+                data['weight'] for _src, _dst, data in G.out_edges(src, data=True)
+            )
+            # dump(src, src_rank, total_weight)
+            for _src, dst, data in G.out_edges(src, data=True):
+                data['rank'] = src_rank * data['weight'] / total_weight
+                ident = data['ident']
+                ranked_definitions[(dst, ident)] += data['rank']
+
+        ranked_tags = []
+        ranked_definitions = sorted(
+            ranked_definitions.items(), reverse=True, key=lambda x: x[1]
+        )
+
+        # dump(ranked_definitions)
+
+        for (fname, ident), rank in ranked_definitions:
+            # print(f"{rank:.03f} {fname} {ident}")
+            ranked_tags += list(definitions.get((fname, ident), []))
+
+        rel_other_fnames_without_tags = set(
+            self.get_relative_fname(fname) for fname in other_fnames
+        )
+
+        fnames_already_included = set(rt[0] for rt in ranked_tags)
+
+        top_rank = sorted(
+            [(rank, node) for (node, rank) in ranked.items()], reverse=True
+        )
+        for rank, fname in top_rank:
+            if fname in rel_other_fnames_without_tags:
+                rel_other_fnames_without_tags.remove(fname)
+            if fname not in fnames_already_included:
+                ranked_tags.append((fname,))
+
+        for fname in rel_other_fnames_without_tags:
+            ranked_tags.append((fname,))
+
+        return ranked_tags
+
+    def get_tags(self, fname, rel_fname):
+        file_mtime = self.get_mtime(fname)
+        if file_mtime is None:
+            return []
+
+        cache_key = fname
+        if (
+            cache_key in self.TAGS_CACHE
+            and self.TAGS_CACHE[cache_key]['mtime'] == file_mtime
+        ):
+            return self.TAGS_CACHE[cache_key]['data']
+
+        # miss!
+
+        data = list(self.get_tags_raw(fname, rel_fname))
+
+        # Update the cache
+        self.TAGS_CACHE[cache_key] = {'mtime': file_mtime, 'data': data}
+        # self.save_tags_cache()
+        return data
+
+    def get_tags_raw(self, fname, rel_fname):
+        lang = filename_to_lang(fname)
+        if not lang:
+            return
+
+        language = get_language(lang)
+        parser = get_parser(lang)
+
+        # Load the tags queries
+        try:
+            scm_fname = (
+                resources.files(__package__)
+                .joinpath('queries')
+                .joinpath(f'tree-sitter-{lang}-tags.scm')
+            )
+        except KeyError:
+            return
+        query_scm = str(scm_fname)
+        if not Path(query_scm).exists():
+            return
+        query_scm = scm_fname.read_text()
+
+        code = self.read_text(fname)
+        if not code:
+            return
+        tree = parser.parse(bytes(code, 'utf-8'))
+
+        # Run the tags queries
+        query = language.query(query_scm)
+        captures = query.captures(tree.root_node)
+
+        captures = list(captures)
+
+        saw = set()
+        for node, tag in captures:
+            if tag.startswith('name.definition.'):
+                kind = 'def'
+            elif tag.startswith('name.reference.'):
+                kind = 'ref'
+            else:
+                continue
+
+            saw.add(kind)
+
+            result = Tag(
+                rel_fname=rel_fname,
+                fname=fname,
+                name=node.text.decode('utf-8'),
+                kind=kind,
+                line=node.start_point[0],
+            )
+
+            yield result
+
+        if 'ref' in saw:
+            return
+        if 'def' not in saw:
+            return
+
+        # We saw defs, without any refs
+        # Some tags files only provide defs (cpp, for example)
+        # Use pygments to backfill refs
+
+        try:
+            lexer = guess_lexer_for_filename(fname, code)
+        except ClassNotFound:
+            return
+
+        tokens = list(lexer.get_tokens(code))
+        tokens = [token[1] for token in tokens if token[0] in Token.Name]
+
+        for token in tokens:
+            yield Tag(
+                rel_fname=rel_fname,
+                fname=fname,
+                name=token,
+                kind='ref',
+                line=-1,
+            )
+
+    def read_text(self, filename):
+        if is_image_file(filename):
+            return self.read_image(filename)
+
+        try:
+            with open(str(filename), 'r', encoding='utf-8') as f:
+                return f.read()
+        except FileNotFoundError:
+            self.log_error(
+                f'Error when constructing repomap: {filename}: file not found error'
+            )
+            return
+        except IsADirectoryError:
+            self.log_error(f'{filename}: is a directory')
+            return
+        except UnicodeError as e:
+            self.log_error(f'{filename}: {e}')
+            self.log_error('Use --encoding to set the unicode encoding.')
+            return
+
+    def read_image(self, filename):
+        try:
+            with open(str(filename), 'rb') as image_file:
+                encoded_string = base64.b64encode(image_file.read())
+                return encoded_string.decode('utf-8')
+        except FileNotFoundError:
+            self.log_error(f'{filename}: file not found error')
+            return
+        except IsADirectoryError:
+            self.log_error(f'{filename}: is a directory')
+            return
+        except Exception as e:
+            self.log_error(f'{filename}: {e}')
+            return
+
+    def log_error(self, message):
+        # FIXME: implement robust logging
+        print(f'Error when constructing repomap: {message}')
+
+    def load_tags_cache(self):
+        path = Path(self.root) / self.TAGS_CACHE_DIR
+        if not path.exists():
+            self.cache_missing = True
+        self.TAGS_CACHE = Cache(path)
 
     def estimate_token_count(self, text):
         """
@@ -179,6 +546,12 @@ class RepoMap:
             )
 
         return self.aider_ignore_spec.match_file(fname)
+
+    def get_mtime(self, fname: str):
+        try:
+            return os.path.getmtime(fname)
+        except FileNotFoundError:
+            self.log_error(f'File not found error: {fname}')
 
     # ================== Path utils ==================
     def get_absolute_path(self, rel_path):
--- openhands/indexing/utils.py
+++ openhands/indexing/utils.py
@@ -1,5 +1,17 @@
 import litellm
 
+IMAGE_EXTENSIONS = {'.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.webp'}
+
+
+def is_image_file(file_name):
+    """
+    Check if a file is an image file based on its extension.
+    Args:
+        file_name (str): The name of the file.
+    """
+    file_name = str(file_name)  # Convert file_name to string
+    return any(file_name.endswith(ext) for ext in IMAGE_EXTENSIONS)
+
 
 def get_token_count_from_text(model_name: str, text: str):
     """
